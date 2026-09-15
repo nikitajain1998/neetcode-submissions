79224505524e5c8527adcb022fcf322ca6b3f8a3
Nikita Jain <nikita.jain@ollion.com>
2026-09-15 21:50:34 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        one = 1
+        two = 2
+
+        for i in range(3,n+1):
+            temp = two
+            two = one+two
+            one = temp
+        return two
+
+        
